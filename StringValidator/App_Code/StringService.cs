1dacc3f46399497e9964484ad684e8e4ae98aa24
unknown <rhuandornelles@gmail.com>
2018-09-27 22:19:55 -0300

    first commit

---
+++ StringValidator/App_Code/StringService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+/// <summary>
+/// Descrição resumida de StringService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+[System.Web.Script.Services.ScriptService]
+public class StringService : System.Web.Services.WebService
+{
+    [WebMethod(EnableSession = true)]
+    [ScriptMethod(UseHttpGet = false)]
+    public string ValidadorString(string validar)
+    {
+        if (Validar(validar))
+        {
+            return $@"Sucesso ao validar a string ""{validar}""";
+        }
+        else
+        {
+            return "String inválida, verifique e tente novamente.";
+        }
+    }
+
+    private bool Validar(string entrada)
+    {
+        if (Regex.IsMatch(entrada, @"\d"))
+        {
+            return false;
+        }
+
+        Dictionary<char, int> dicionario = new Dictionary<char, int>();
+
+        foreach (char c in "{}[]()")
+        {
+            dicionario.Add(c, 0);
+        }
+
+        foreach (char c in entrada)
+        {
+            if (dicionario.ContainsKey(c)) dicionario[c] += 1;
+        }
+
+        return dicionario['{'] == dicionario['}']
+            && dicionario['('] == dicionario[')']
+            && dicionario['['] == dicionario[']'];
+    }
+
+}
---
+++ StringValidator/js/funcoes.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $(document).on('click', '#submit',function (e) {
+        e.preventDefault();
+        var string = $('#Validar').val();
+        if ($.trim(string) == "") {
+            alert("Escreva algo, por favor, senão este algoritmo não tem sentido.");
+            return false;
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: '../WebServices/StringService.asmx/ValidadorString',
+                data: JSON.stringify({ validar: string }),
+                contentType: "application/json; charset=utf-8",
+                success: function (ret) {
+                    alert(ret.d);
+                },
+                error: function (status, error, e) {
+                    console.log(error);
+                    console.log(status);
+                    console.log(e);
+                    alert("Erro ao fazer a verificação. Contate os desenvolvedores!");
+                }
+            });
+        }
+    });
+});
